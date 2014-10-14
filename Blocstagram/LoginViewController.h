a645f58bf6dc641463705598251bab4d491b898f
Jonathan Jungemann <jrjungemann@gmail.com>
2014-10-14 14:17:12 -0500

    Add Instagram Login

---
+++ Blocstagram/LoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.h
+//  Blocstagram
+//
+//  Created by Jon Jungemann on 10/14/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+extern NSString *const LoginViewControllerDidGetAccessTokenNotification;
+
+@end
